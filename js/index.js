e55ed610fd6b0c9ea51939d599b23c1e8414bfb4
keronmat <keronmat@live.com>
2019-03-15 22:46:46 -0400

    Added a digital clock with some features

--- js/index.js
+++ js/index.js
@@ -2,10 +2,19 @@ const secondHand = document.querySelector(".secondhand");
 const minuteHand = document.querySelector(".minutehand");
 const hourHand = document.querySelector(".hourhand");
 const time = document.querySelector(".time");
+const digiTime = document.querySelector(".digital-time");
+const digiSeconds = document.querySelector(".digital-seconds");
 const day = document.querySelector(".day");
 const month = document.querySelector(".month");
 const year = document.querySelector(".year");
 const amPm = document.querySelector(".amPm");
+const digitalDate = document.querySelector(".digital-date");
+const digitalOpenAlarmButton = document.querySelector(".open-alarm");
+const snoozeButton = document.querySelector(".snoozeButton");
+const closeAlarm = document.querySelector(".close-alarm");
+const setAlarmButton = document.querySelector("#alarmButton");
+const bellButtonOff = document.querySelector(".fa-bell-slash");
+const alarmForm = document.querySelector(".alarm-details");
 
 const setTime = () => {
   const now = new Date();
@@ -24,14 +33,28 @@ const setTime = () => {
   const hours = now.getHours();
   const hoursDegrees = (hours / 12) * 360 + 270;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-  //console.log(hours);
-  //adds digital time to clock
+
+  //adds digital time to analog clock and digital clock
   let newMins = minutes.toString().length > 1 ? minutes : `0${minutes}`;
   time.innerHTML = `${convertHours(hours)}:${newMins}`;
+  digiTime.innerHTML = `${convertHours(hours)}:${newMins}`;
+
+  digiSeconds.innerHTML = `:${convertSeconds(seconds)}`;
 
   //check to see if its AM or PM
   const timeOfTheDay = hours < 12 || hours === 24 ? "AM" : "PM";
   amPm.innerHTML = timeOfTheDay;
+  addAmPmToDigital(timeOfTheDay);
+
+  //get date to add to digital clock'
+
+  addDateToDigital(
+    now.getDay(),
+    now.getDate(),
+    now.getMonth(),
+    now.getFullYear()
+  );
+
   changeBackGroundImg(hours);
   playSound();
 };
@@ -42,6 +65,151 @@ const convertHours = hours => {
   newHours = newHours.length = 2 ? newHours : `0${newHours}`;
   return newHours;
 };
+const convertSeconds = secs => {
+  let newSecs = secs.toString();
+  newSecs = newSecs.length == 1 ? `0${newSecs}` : newSecs;
+
+  return newSecs;
+};
+
+//add AM and PM to digital clock
+const addAmPmToDigital = timeOfTheDay => {
+  const AM = document.querySelector("#am");
+  const PM = document.querySelector("#pm");
+  if (timeOfTheDay === "PM") {
+    PM.classList.add("digital-pm");
+  } else if (timeOfTheDay === "AM") {
+    AM.classList.add("digital-am");
+  }
+};
+
+// Add date to digital clock
+
+const addDateToDigital = (day, date, monIndex, year) => {
+  let month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  let end = "";
+
+  if (date > 3 && date < 21) end = "th";
+  switch (date % 10) {
+    case 1:
+      end = "st";
+    case 2:
+      end = "nd";
+    case 3:
+      end = "rd";
+    default:
+      end = "th";
+  }
+  const daySpans = Array.from(
+    document.querySelectorAll(".digital-weekdays>span")
+  );
+  daySpans[day].classList.add("today");
+  digitalDate.innerHTML = `${date}${end.sup()} ${month[monIndex]}, ${year}`;
+};
+
+//  alarm clock setup starts
+let alarmTimer; //
+
+const openAlarm = () => {
+  const x = document.querySelector(".alarm-details");
+  const offBell = document.querySelector(".close-alarm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    offBell.style.display = "inline-block";
+  } else {
+    x.style.display = "none";
+    offBell.style.display = "none";
+  }
+};
+
+const playAlarmSound = () => {
+  const audio = document.querySelector(`#alarmSound`);
+  audio.play();
+};
+
+const setAlarm = () => {
+  const setAlarmTimeInMs = document.getElementById("alarmTime").valueAsNumber;
+  const bellButtonOn = document.querySelector(".fa-bell");
+  const alarmOptionsButton = document.querySelector("#alarmOptions");
+
+  if (isNaN(setAlarmTimeInMs)) {
+    alert("Invalid Date");
+    return;
+  }
+  const alarm = new Date(setAlarmTimeInMs);
+  const alarmTime = new Date(
+    alarm.getUTCFullYear(),
+    alarm.getUTCMonth(),
+    alarm.getUTCDate(),
+    alarm.getUTCHours(),
+    alarm.getUTCMinutes(),
+    alarm.getUTCSeconds()
+  );
+
+  const differenceInTime = alarmTime.getTime() - new Date().getTime();
+
+  if (differenceInTime < 0) {
+    alert("Time has already passed");
+    return;
+  } else {
+    bellButtonOn.style.color = "var(--alarmOn)";
+    alarmTimer = setTimeout(initAlarm, differenceInTime);
+    alarmOptionsButton.style.display = "inline-block";
+  } //
+};
+
+const initAlarm = () => {
+  const audio = document.querySelector(`#alarmSound`);
+  audio.play();
+
+  document.querySelector(".open-alarm>i").classList.add("alarmGoing");
+};
+
+const stopCancelAlarm = () => {
+  const bellButtonOn = document.querySelector(".fa-bell");
+  const alarmOptions = document.getElementById("alarmOptions");
+  const audio = document.querySelector(`#alarmSound`);
+
+  audio.pause();
+  audio.crrentTime = 0;
+
+  alarmOptions.style.display = "none";
+  clearTimeout(alarmTimer);
+  alarmForm.reset();
+  bellButtonOn.style.color = "var(--platinum)";
+  document.querySelector(".open-alarm>i").classList.remove("alarmGoing");
+  openAlarm();
+};
+
+function snooze() {
+  const bellButtonOn = document.querySelector(".fa-bell");
+  const alarmOptions = document.getElementById("alarmOptions");
+
+  stopCancelAlarm();
+  bellButtonOn.style.color = "var(--gold)";
+  alarmOptions.style.display = "inline-block";
+
+  setTimeout(initAlarm, 300000);
+}
+
+digitalOpenAlarmButton.addEventListener("click", openAlarm);
+snoozeButton.addEventListener("click", snooze);
+setAlarmButton.addEventListener("click", setAlarm);
+closeAlarm.addEventListener("click", stopCancelAlarm);
+//end alarm
 // changes background image depending the hour of the day
 const changeBackGroundImg = hours => {
   const body = document.querySelector("body");
@@ -63,11 +231,11 @@ const changeBackGroundImg = hours => {
 
 //add ticking sound to the clock
 function playSound() {
-  const audio = document.querySelector(`audio`);
+  const audio = document.querySelector(`#tickingSound`);
 
   if (!audio) return;
   audio.currentTime = 0;
-  audio.volume = 0.5;
+  audio.volume = 0.2;
   audio.play();
 }
 setInterval(setTime, 1000);
